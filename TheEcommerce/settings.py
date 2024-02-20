985d759109e47251b18c5485c86b35ffb491f8f8
haseeb4339 <haseebullahbj@gmail.com>
2024-02-20 21:59:50 +0500

    First commit

--- TheEcommerce/settings.py
+++ TheEcommerce/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'TheEcommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR /"static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
