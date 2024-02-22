ebcc9f54c9b6193bd940e74986fd5df3588d4411
haseeb4339 <haseebullahbj@gmail.com>
2024-02-22 16:27:55 +0500

    Create a custom manager

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,31 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username,email, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        
+        if not username:
+            raise ValueError("User must have a username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
 
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
