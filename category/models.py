66435cdc2f762b0224bad0fdc1294653ebf23ff9
haseeb4339 <haseebullahbj@gmail.com>
2024-02-22 19:23:02 +0500

    prepopulate

--- category/models.py
+++ category/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length = 50, unique = True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, blank = True)
     cat_img = models.ImageField(upload_to='photos/categories')
 
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+        # db_table = 'category'  # Specify the desired table name here
 
     def __str__(self):
         return self.category_name
