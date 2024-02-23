ee4b2bc53d8327f98babaac147974962bde48566
haseeb4339 <haseebullahbj@gmail.com>
2024-02-23 17:07:43 +0500

    display all the categoires on store page

--- store/views.py
+++ store/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 
 from .models import Product
 
+from category.models import Category
+
 
 def store(request):
 
     products = Product.objects.all().filter(is_available=True)
+    
+    products_count = products.count()
+    categories = Category.objects.all()
 
     context = {
-        'products': products
+        'products': products,
+        'products_count':products_count,
+        'categories': categories,
     }
 
     return render(request, 'store/store.html', context)
+
+
