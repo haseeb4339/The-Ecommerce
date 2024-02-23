b5ae339ed5b65dd80e632b031512c84f01ce3a07
haseeb4339 <haseebullahbj@gmail.com>
2024-02-23 16:52:13 +0500

    display all the product from database to store page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Product
+
 
 def store(request):
 
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'store/store.html', context)
