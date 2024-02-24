d39b16c584530c749e420113a4e887a4656b9691
haseeb4339 <haseebullahbj@gmail.com>
2024-02-25 00:24:50 +0500

    changing in the store view

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from .models import Product
 from category.models import Category
 
 
-def store(request, category_slug):
+def store(request, category_slug=None):
 
     categories = None
     product = None
@@ -29,13 +29,7 @@ def store(request, category_slug):
 
     return render(request, 'store/store.html', context)
 
-def display_category(request, category_slug=None):
-    categories = None
-    product = None
-    if category_slug is not None:
-        categories = get_object_or_404(Category, category_slug)
-        products = Product.objects.filter(Category=categories, is_available=True)
-        products_count = products.count()
+
 
 
 
