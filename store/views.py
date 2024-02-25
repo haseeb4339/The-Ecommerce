11791705f7960ccf05600ffd7199368c4daea33d
haseeb4339 <haseebullahbj@gmail.com>
2024-02-25 23:08:27 +0500

    remove some error

--- store/views.py
+++ store/views.py
@@ -32,6 +32,18 @@ def store(request, category_slug=None):
 
 def product_detail(request, category_slug, product_slug):
 
-   
+    single_product = None
 
-    return render(request, 'store/product_detail.html', {})
+    try:
+
+        single_product = Product.objects.get(category__slug=category_slug, slug = product_slug)
+
+    except Exception as e:
+        raise e
+       
+
+    context = {
+        'single_product': single_product
+    }   
+
+    return render(request, 'store/product_detail.html',context)
