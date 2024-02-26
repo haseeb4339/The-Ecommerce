c3e65a2d44669521e1bf7a15090e9c531bd2f986
haseeb4339 <haseebullahbj@gmail.com>
2024-02-26 23:12:12 +0500

    render the cart details

--- carts/views.py
+++ carts/views.py
@@ -32,10 +32,28 @@ def add_cart(request, product_id):
 
         )
         cart_item.save()
+
     
     return redirect('cart')
 
 
-def cart(request):
+def cart(request, total = 0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_add(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    
+    except:
+        pass
 
-    return render(request, 'store/cart.html')
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items
+
+    }
+
+
+    return render(request, 'store/cart.html', context)
