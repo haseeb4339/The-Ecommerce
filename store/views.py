068982005edcf0d005c68517b527d4121e0bfcdc
haseeb4339 <haseebullahbj@gmail.com>
2024-02-27 23:24:55 +0500

    make change in the product page

--- store/views.py
+++ store/views.py
@@ -39,15 +39,16 @@ def product_detail(request, category_slug, product_slug):
     try:
 
         single_product = Product.objects.get(category__slug=category_slug, slug = product_slug)
-        is_cart = CartItem.objects.filter(cart__cart_id=_cart_add(request), product=single_product).exists()
-        
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_add(request), product=single_product).exists()
+
 
     except Exception as e:
         raise e
        
 
     context = {
-        'single_product': single_product
+        'single_product': single_product,
+        'in_cart':in_cart,
     }   
 
     return render(request, 'store/product_detail.html',context)
