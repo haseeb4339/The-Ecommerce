ee3a726dfd42d923db5e0dab5992b1fbf002b353
haseeb4339 <haseebullahbj@gmail.com>
2024-02-27 21:16:16 +0500

    create remove cart function

--- carts/views.py
+++ carts/views.py
@@ -51,6 +51,14 @@ def remove_cart(request, product_id):
     return redirect('cart')
 
 
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_add(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
 
 def cart(request, total = 0, quantity=0, cart_items=None):
     try:
