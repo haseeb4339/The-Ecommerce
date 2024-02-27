931e6bdb9e2c3f16871490e06d2dfd7884e22d12
haseeb4339 <haseebullahbj@gmail.com>
2024-02-27 20:05:00 +0500

    create tax and grand total and render on the cart page

--- carts/views.py
+++ carts/views.py
@@ -44,6 +44,9 @@ def cart(request, total = 0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+
+        tax = (2 * total)/100
+        grand_total = total+tax
     
     except:
         pass
@@ -51,7 +54,10 @@ def cart(request, total = 0, quantity=0, cart_items=None):
     context = {
         'total':total,
         'quantity':quantity,
-        'cart_items':cart_items
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+
 
     }
 
