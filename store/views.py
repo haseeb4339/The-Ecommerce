6e5715cebf91efa3ebc01ff00b2cea7b4e6ad4b6
haseeb4339 <haseebullahbj@gmail.com>
2024-02-27 23:18:18 +0500

    create a check product is already added to cart or not

--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_add
 
 
 def store(request, category_slug=None):
@@ -37,6 +39,8 @@ def product_detail(request, category_slug, product_slug):
     try:
 
         single_product = Product.objects.get(category__slug=category_slug, slug = product_slug)
+        is_cart = CartItem.objects.filter(cart__cart_id=_cart_add(request), product=single_product).exists()
+        
 
     except Exception as e:
         raise e
