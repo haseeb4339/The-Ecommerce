dbe1fc62e0b175e568b6ccd66d2621f17c3c5865
haseeb4339 <haseebullahbj@gmail.com>
2024-02-28 19:36:41 +0500

    add functionality of paginator in template

--- store/views.py
+++ store/views.py
@@ -24,7 +24,7 @@ def store(request, category_slug=None):
 
         products = Product.objects.all().filter(is_available=True)
 
-        paginator = Paginator(products,6)
+        paginator = Paginator(products,3)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
 
