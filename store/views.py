78b70ea79906a15fa02d4d38bd528885d6a471d2
haseeb4339 <haseebullahbj@gmail.com>
2024-02-28 20:46:21 +0500

    create a search functionality

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_add
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 
 def store(request, category_slug=None):
@@ -61,3 +62,17 @@ def product_detail(request, category_slug, product_slug):
     }   
 
     return render(request, 'store/product_detail.html',context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products= Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            products_count = products.count()
+    context = {
+        'products':products,
+        'products_count':products_count,
+    }
+
+    return render(request, 'store/store.html', context)
