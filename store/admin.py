5e1a314f7af7f6da597039e90ef25343f74bdc8d
haseeb4339 <haseebullahbj@gmail.com>
2024-02-29 16:12:41 +0500

    create a model for varation and register it to admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Variation
 
 
 
@@ -8,4 +8,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug':('product_name',) }
 
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variaton_category', 'variaton_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = list_display = ('product', 'variaton_category', 'variaton_value')
+
+
+
+
+
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
