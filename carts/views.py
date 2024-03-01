22ab09fa2aeab349cb669b18d86082d2da29faff
haseeb4339 <haseebullahbj@gmail.com>
2024-03-01 21:39:22 +0500

    group the selected item in cart

--- carts/views.py
+++ carts/views.py
@@ -32,12 +32,12 @@ def add_cart(request, product_id):
     cart.save()
 
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.create(product=product,quantity = 1, cart=cart)
         if len(product_varitaion) > 0:
             for item in product_varitaion:
                 cart_item.variations.clear()
                 cart_item.variations.add(item)
-        cart_item.quantity +=1
+        # cart_item.quantity +=1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
