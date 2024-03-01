d30cdac11b390acbad3f76baeeddc2401611ef6e
haseeb4339 <haseebullahbj@gmail.com>
2024-03-01 18:40:10 +0500

    create a display for cartitem

--- carts/admin.py
+++ carts/admin.py
@@ -5,5 +5,8 @@ from .models import Cart, CartItem
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
 
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+
 admin.site.register(Cart, CartAdmin)
-admin.site.register(CartItem)
+admin.site.register(CartItem, CartItemAdmin)
